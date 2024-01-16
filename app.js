28b16da02265c8f6ba040c187cfc143adbd4bf78
Kasun Sanjaya <100067563+Kasun-Sanjaya@users.noreply.github.com>
2024-01-16 20:05:05 +0530

    change app display

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
-/**
- * What i learnt-
- * Modules - creation,public and private methods and properties, seperation of concerns
- * keypress events,keycodes
- * reading data from different html input types
- * storing hardcoded strings(such as classnames) in objects, and accessing them through the object in the entire app
- * Initialization function - so the module contains only functions
- * How to choose relevant function constructors
- * How to setup proper data structure for our program
- * How to add big chunks of html inro the dom
- * How to replace parts of string
- * DOM manipulation using the insertAdjacentHtml method
- * Clearing Html feilds
- * using querySelectorAll
- * Converting list to an array
- * foreach loop
- * Convert input feilds to numbers, prevent false inputs
- * Event delgation in practice
- * How to use Id's in html to connect the ui with data model
- * How to use the prentNode property for Dom traversing
- * Looping over an array through map
- * Remove elements from an array using the splice method
- * creating forEach function for nodeLists
- * getting the current date through Date object constructor
- * the change event
- */
 
-//Modules return methods that they want to be public.
-
-//BUDGET CONTROLLER
 var budgetController = (function() {
   //function constructors
   var Expense = function(id, description, value) {
